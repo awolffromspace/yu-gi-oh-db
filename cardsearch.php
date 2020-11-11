e36e4b4a8139911616875c35858336ea8c5a5171
Eric Thomas <ewolft@gmail.com>
2020-11-10 19:48:26 -0500

    Update search form

--- cardsearch.php
+++ cardsearch.php
@@ -30,51 +30,22 @@ if (isset($_SESSION['user']))
 		<?php include('header.php'); require('model/connectdb.php'); ?>
 		
         <form>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">Email</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
-    </div>
-    <div class="form-group col-md-6">
-      <label for="inputPassword4">Password</label>
-      <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="inputAddress">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-  </div>
-  <div class="form-group">
-    <label for="inputAddress2">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-  </div>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputCity">City</label>
-      <input type="text" class="form-control" id="inputCity">
-    </div>
-    <div class="form-group col-md-4">
-      <label for="inputState">State</label>
-      <select id="inputState" class="form-control">
-        <option selected>Choose...</option>
-        <option>...</option>
-      </select>
-    </div>
-    <div class="form-group col-md-2">
-      <label for="inputZip">Zip</label>
-      <input type="text" class="form-control" id="inputZip">
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div>
-  </div>
-  <button type="submit" class="btn btn-primary">Sign in</button>
-</form>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="cardID">Card ID</label>
+                    <input type="text" class="form-control" id="cardID" placeholder="id">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="name">Card Name</label>
+                    <input type="text" class="form-control" id="name" placeholder="name">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="type">Card Type</label>
+                    <input type="text" class="form-control" id="type" placeholder="type">
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Search</button>
+        </form>
 
 <?php
 //close bracket from the "if" from before
