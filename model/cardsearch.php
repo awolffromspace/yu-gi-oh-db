3539bbf2aa9bd78f7192dcffa1611b647ceec674
Eric Thomas <ewolft@gmail.com>
2020-11-10 21:39:30 -0500

    Testing

--- model/cardsearch.php
+++ model/cardsearch.php
@@ -1,14 +1,14 @@
 <?php
     function cardSearch($cardID, $name, $type, $race, $attribute, $level, $atk, $def, $cardDesc) {
         require('connectdb.php');
-        $query = "SELECT * FROM Card_name WHERE type = :type";
+        $query = "SELECT * FROM Card_name";
 
         $statement = $db->prepare($query);
 
-        $statement->bindValue(':cardID', $cardID);
-        $statement->bindValue(':name', $name);
-        $statement->bindValue(':type', $type);
-        $statement->bindValue(':race', $race);
+        //$statement->bindValue(':cardID', $cardID);
+        //$statement->bindValue(':name', $name);
+        //$statement->bindValue(':type', $type);
+        //$statement->bindValue(':race', $race);
         
         $statement->execute();
         
