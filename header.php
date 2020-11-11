f1964c0c8fa7c6c64800ff6f4bb49b0ea77d826e
Eric Thomas <ewolft@gmail.com>
2020-11-10 19:26:49 -0500

    Update navbar

--- header.php
+++ header.php
@@ -1,17 +1,13 @@
 <!-- Author: Peter Chen -->
         <header>
             <nav>
-                <a href="profile.php" id="site-name">Yu Gi Oh DB</a>
+                <a href="." id="site-name">Yu Gi Oh DB</a>
                 <ul class="nav">
-                    <li><a href="input.php"><i class="fas fa-clock"></i>Enter Hours</a></li>
-                    <li><a href="profile.php"><i class="fas fa-user"></i>Profile</a></li>
-                 <!-- 
-					I think this part might be to differentiate the different users from each other
-					<li>
-                        <a href=<?= "http://localhost:4200?page=account&user=" . $_SESSION['user'] ?> >
-                        <i class="fas fa-address-card"></i>Account</a>
-                    </li> -->
-                    <li><a href="logout.php"><i class="fas fa-sign-in-alt"></i>Logout</a></li>
+                    <li><a href="."></i>Home Page</a></li>
+                    <li><a href="."></i>Settings</a></li>
+                    <li><a href="."></i>Search Cards</a></li>
+                    <li><a href="."></i>Search Players</a></li>
+                    <li><a href="."></i>Log Out</a></li>
                 </ul>
             </nav>
         </header>
