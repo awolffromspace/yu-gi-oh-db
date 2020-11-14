c5762105cdf900443ad584452f0acf6479622ee4
Eric Thomas <ewolft@gmail.com>
2020-11-14 14:33:25 -0500

    Debugging

--- model/cardsearch.php
+++ model/cardsearch.php
@@ -1,7 +1,53 @@
 <?php
     function cardSearch($card_ID, $name, $type, $race, $card_attribute, $level, $atk, $def, $card_desc) {
         require('connectdb.php');
-        $query = "SELECT * FROM Cards NATURAL JOIN Card_name NATURAL JOIN Card_image NATURAL LEFT JOIN Monster WHERE name=:name";
+        $query = "SELECT * FROM Cards NATURAL JOIN Card_name NATURAL JOIN Card_image NATURAL LEFT JOIN Monster WHERE";
+
+        if ($card_ID == "card_ID") {
+            $query = $query." card_ID=card_ID";
+        } else {
+            $query = $query." card_ID=:card_ID";
+        }
+        if ($name == "name") {
+            $query = $query." AND name=name";
+        } else {
+            $query = $query." AND name=:name";
+        }
+        if ($type == "type") {
+            $query = $query." AND type=type";
+        } else {
+            $query = $query." AND type=:type";
+        }
+        if ($race == "race") {
+            $query = $query." AND race=race";
+        } else {
+            $query = $query." AND race=:race";
+        }
+        if ($card_attribute == "card_attribute") {
+            $query = $query." AND card_attribute=card_attribute";
+        } else {
+            $query = $query." AND card_attribute=:card_attribute";
+        }
+        if ($level == "level") {
+            $query = $query." AND level=level";
+        } else {
+            $query = $query." AND level=:level";
+        }
+        if ($atk == "atk") {
+            $query = $query." AND atk=atk";
+        } else {
+            $query = $query." AND atk=:atk";
+        }
+        if ($def == "def") {
+            $query = $query." AND def=def";
+        } else {
+            $query = $query." AND def=:def";
+        }
+        if ($atk == "card_desc") {
+            $query = $query." AND card_desc=card_desc";
+        } else {
+            $query = $query." AND card_desc=:card_desc";
+        }
 
         $statement = $db->prepare($query);
 
