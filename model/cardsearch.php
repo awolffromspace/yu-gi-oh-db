8d8a8b1e880304bdd2d78bf5975bd3116de1a779
Eric Thomas <ewolft@gmail.com>
2020-11-14 15:19:14 -0500

    Debugging

--- model/cardsearch.php
+++ model/cardsearch.php
@@ -51,26 +51,36 @@
 
         $statement = $db->prepare($query);
 
-        //$statement->bindValue(':card_ID', intval($card_ID), PDO::PARAM_INT);
-        $statement->bindValue(':name', $name, PDO::PARAM_STR);
-        $statement->bindValue(':type', $type, PDO::PARAM_STR);
-        $statement->bindValue(':race', $race, PDO::PARAM_STR);
-        $statement->bindValue(':card_attribute', $card_attribute, PDO::PARAM_STR);
-        //$statement->bindValue(':level', intval($level), PDO::PARAM_INT);
-        //$statement->bindValue(':atk', intval($atk), PDO::PARAM_INT);
-        //$statement->bindValue(':def', intval($def), PDO::PARAM_INT);
-        $statement->bindValue(':card_desc', $card_desc, PDO::PARAM_STR);
+        if ($card_ID != "card_ID") {
+            $statement->bindValue(':card_ID', intval($card_ID), PDO::PARAM_INT);
+        }
+        if ($name != "name") {
+            $statement->bindValue(':name', $name, PDO::PARAM_STR);
+        }
+        if ($type != "type") {
+            $statement->bindValue(':type', $type, PDO::PARAM_STR);
+        }
+        if ($race != "race") {
+            $statement->bindValue(':race', $race, PDO::PARAM_STR);
+        }
+        if ($card_attribute != "card_attribute") {
+            $statement->bindValue(':card_attribute', $card_attribute, PDO::PARAM_STR);
+        }
+        if ($level != "level") {
+            $statement->bindValue(':level', intval($level), PDO::PARAM_INT);
+        }
+        if ($atk != "atk") {
+            $statement->bindValue(':atk', intval($atk), PDO::PARAM_INT);
+        }
+        if ($def != "def") {
+            $statement->bindValue(':def', intval($def), PDO::PARAM_INT);
+        }
+        if ($card_desc != "card_desc") {
+            $statement->bindValue(':card_desc', $card_desc, PDO::PARAM_STR);
+        }
 
         echo $query;
-        echo $card_ID;
-        echo $name;
-        echo $type;
-        echo $race;
-        echo $card_attribute;
-        echo $level;
-        echo $atk;
-        echo $def;
-        echo $card_desc;
+        echo "\n";
 
         $statement->execute();
 
