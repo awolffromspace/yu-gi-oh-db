b5dfc5d849b0f1ecce61fd9011a86c8389b6e9d2
Eric Thomas <ewolft@gmail.com>
2020-11-14 13:42:15 -0500

    Fix string syntax

--- model/cardsearch.php
+++ model/cardsearch.php
@@ -1,7 +1,7 @@
 <?php
     function cardSearch($card_ID, $name, $type, $race, $card_attribute, $level, $atk, $def, $card_desc) {
         require('connectdb.php');
-        $query = "SELECT * FROM Cards NATURAL JOIN Card_name NATURAL JOIN Card_image NATURAL JOIN Monster WHERE card_ID=:card_ID AND name=:name AND type=:type AND race=:race AND card_attribute=:card_attribute AND level=:level AND atk=:atk AND def=:def AND card_desc=:card_desc";
+        $query = "SELECT * FROM Cards NATURAL JOIN Card_name NATURAL JOIN Card_image NATURAL JOIN Monster WHERE card_ID=:card_ID AND name=':name' AND type=':type' AND race=':race' AND card_attribute=':card_attribute' AND level=:level AND atk=:atk AND def=:def AND card_desc=':card_desc'";
 
         $statement = $db->prepare($query);
 
