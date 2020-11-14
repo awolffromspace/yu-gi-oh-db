b61e65be24bf8af8a3101a56abe1555735c2d75e
Eric Thomas <ewolft@gmail.com>
2020-11-14 16:55:12 -0500

    Testing redirect

--- card-search.php
+++ card-search.php
@@ -27,7 +27,6 @@ if (isset($_SESSION['user']))
 ?>
  
     <body>
-		<?php include('header.php'); ?>
         <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST" name="cardSearch">
             <div class="form-row">
                 <div class="form-group col-md-4">
@@ -129,6 +128,15 @@ if (isset($_SESSION['user']))
 
         <?php
             if ($_SERVER['REQUEST_METHOD'] == "POST") {
+                $_SESSION['search_card_ID'] = trim($_POST['card_ID']);
+                $_SESSION['search_name'] = trim($_POST['name']);
+                $_SESSION['search_type'] = trim($_POST['type']);
+                $_SESSION['search_race'] = trim($_POST['race']);
+                $_SESSION['search_card_attribute'] = trim($_POST['card_attribute']);
+                $_SESSION['search_level'] = trim($_POST['level']);
+                $_SESSION['search_atk'] = trim($_POST['atk']);
+                $_SESSION['search_def'] = trim($_POST['def']);
+                $_SESSION['search_card_desc'] = trim($_POST['card_desc']);
                 header('Location: profile.php');
             }
         ?>
