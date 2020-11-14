677c6e91ea2fda8023604576cff33d9c88376232
jn3ng <74276640+jn3ng@users.noreply.github.com>
2020-11-14 15:09:15 -0500

    Update header.php

--- header.php
+++ header.php
@@ -5,8 +5,7 @@
                 <ul class="nav">
                     <li><a href="profile.php"></i>Home Page</a></li>
                     <li><a href="settings.php"></i>Settings</a></li>
-                    <li><a href="."></i>Search Cards</a></li>
-                    <li><a href="."></i>Search Players</a></li>
+                    <li><a href="cardsearch.php"></i>Search Cards</a></li>
                     <li><a href="logout.php"></i>Log Out</a></li>
                 </ul>
             </nav>
