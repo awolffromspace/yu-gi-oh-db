76d73bd4acb207e1281d9d4b3e03cfb6bf798805
Eric Thomas <ewolft@gmail.com>
2020-11-14 14:03:04 -0500

    Debugging

--- model/cardsearch.php
+++ model/cardsearch.php
@@ -1,7 +1,7 @@
 <?php
     function cardSearch($card_ID, $name, $type, $race, $card_attribute, $level, $atk, $def, $card_desc) {
         require('connectdb.php');
-        $query = "SELECT * FROM Cards NATURAL JOIN Card_name NATURAL JOIN Card_image NATURAL LEFT JOIN Monster WHERE card_ID=:card_ID";
+        $query = "SELECT * FROM Cards NATURAL JOIN Card_name NATURAL JOIN Card_image NATURAL LEFT JOIN Monster WHERE card_ID=:card_ID AND name=:name AND type=:type AND race=:race AND card_attribute=:card_attribute AND level=:level AND atk=:atk AND def=:def AND card_desc=:card_desc";
 
         $statement = $db->prepare($query);
 
@@ -17,6 +17,7 @@
 
         echo $query;
         echo $card_ID;
+        echo is_numeric($card_ID)
         echo $name;
         echo $type;
         echo $race;
